4fda6b8279c02cbf8971413d5fa613dce145ee34
Alan Burstein <alanjburstein@gmail.com>
2017-12-14 22:03:25 -0600

    patExclude8k and patExclude8k_2017

--- dbo.patExclude8K_2017.sql
+++ dbo.patExclude8K_2017.sql
@@ -1,27 +1,31 @@
-IF OBJECT_ID('dbo.patExclude8K', 'IF') IS NOT NULL DROP FUNCTION dbo.patExclude8K;
+IF OBJECT_ID('dbo.patExclude8K_2017', 'IF') IS NOT NULL 
+  DROP FUNCTION dbo.patExclude8K_2017;
 GO
-CREATE FUNCTION dbo.patExclude8K
+CREATE FUNCTION dbo.patExclude8K_2017
 (
   @string  varchar(8000),
 	@pattern varchar(50)
 ) 
 /*****************************************************************************************
-Purpose:
- Given a string (@string) and a pattern (@pattern) patExclude8k removes any character in
- @string that matches the @pattern. 
+Note:
+ This function only runs on SQL Server 2017 or higher
 
-Compatibility: 
- SQL Server 2008+; can be modified for 2005 compatibility using ngrams8k_2005
+Purpose:
+ Given a string (@string) and a pattern (@pattern) patExclude8K_2017 removes any character
+ in @string that matches the @pattern.
+
+Compatibility:
+ SQL Server 2017+ (Leverages STRING_AGG)
 
 Syntax:
 --===== Basic Syntax (against parameters)
  SELECT px.cleanedString
- FROM dbo.patExclude8K(@string,@pattern) px;
+ FROM dbo.patExclude8K_2017(@string,@pattern) px;
 
 --===== Basic Syntax (against a table)
  SELECT px.cleanedString
  FROM dbo.someTable t
- CROSS APPLY dbo.patExclude8K(t.someString,@pattern) px;
+ CROSS APPLY dbo.patExclude8K_2017(t.someString,@pattern) px;
 
 Parameters:
   @string  = varchar(8000); input string to clean
@@ -37,45 +41,46 @@ Usage:
 --===== Remove all but alpha characters
  SELECT px.cleanedString 
  FROM dbo.SomeTable st
- CROSS APPLY dbo.patExclude8K(st.SomeString,'%[^0-9a-zA-Z]%') px;
+ CROSS APPLY dbo.patExclude8K_2017(st.SomeString,'%[^0-9a-zA-Z]%') px;
 
 --===== Remove all but numeric characters and dots
  SELECT CleanedString
  FROM dbo.SomeTable st
- CROSS APPLY dbo.PatExclude8K(st.SomeString,'%[^0-9.]%');
+ CROSS APPLY dbo.patExclude8K_2017(st.SomeString,'%[^0-9.]%');
 
 ----------------------------------------------------------------------------------------
 Runnable Examples:
 	
 --remove letters
- SELECT newString FROM dbo.PatExclude8K('abc123!', '[a-z]'); -- Returns: 123!
+ SELECT newString FROM dbo.patExclude8K_2017('abc123!', '[a-z]'); -- Returns: 123!
 
 -- remove numbers
- SELECT newString FROM dbo.PatExclude8K('abc123!', '[0-9]'); -- Returns: abc!
+ SELECT newString FROM dbo.patExclude8K_2017('abc123!', '[0-9]'); -- Returns: abc!
 
 -- only return letters and numbers
- SELECT newString FROM dbo.PatExclude8K('###abc123!!!', '[^0-9a-z]');
+ SELECT newString FROM dbo.patExclude8K_2017('###abc123!!!', '[^0-9a-z]');
 Returns: abc123
 
 -- Remove spaces
- SELECT newString FROM dbo.PatExclude8K('XXX 123 ZZZ', ' '); -- Returns: XXX123ZZZ
+ SELECT newString FROM dbo.patExclude8K_2017('XXX 123 ZZZ', ' '); -- Returns: XXX123ZZZ
 
 -- only return letters and "!, ? or ."
  SELECT newString 
- FROM dbo.PatExclude8K('123# What?!... ', '[^A-Za-z!?.]')  -- Returns: What?!...
-
+ FROM dbo.patExclude8K_2017('123# What?!... ', '[^A-Za-z!?.]')  -- Returns: What?!...
 ----------------------------------------------------------------------------------------
 Developer Notes:
 
- 1. Requires nGrams8k located here: https://goo.gl/T3DDiY
+ 1. Requires nGrams8k located here: https://goo.gl/T3DDiY. 
+ 
+ 2. patExclude8K_2017 is identical to patExclude8K but leverages SQL Server 2017's 
+    STRING_AGG (https://goo.gl/G7Urrb) for concatination
 
- 2. This function is what is referred to as an "inline" scalar UDF." Technically it's an
-    inline table valued function (iTVF) but performs the same task as a scalar valued user
-    defined function (UDF); the difference is that it requires the APPLY table operator
-    to accept column values as a parameter. For more about "inline" scalar UDFs see this
-    article by SQL MVP Jeff Moden: http://www.sqlservercentral.com/articles/T-SQL/91724/
-    and for more about how to use APPLY see the this article by SQL MVP Paul White:
-    http://www.sqlservercentral.com/articles/APPLY/69953/.
+ 3. patExclude8K_2017 is what is referred to as an "inline" scalar UDF." Technically it's 
+    an inline table valued function (iTVF) but performs the same task as a scalar valued 
+    user defined function (UDF); the difference is that it requires the APPLY table 
+    operator to accept column values as a parameter. For more about "inline" scalar UDFs 
+    see this article by SQL MVP Jeff Moden: https://goo.gl/vTYf1S; and for more about how 
+    to use APPLY see the this article by SQL MVP Paul White: https://goo.gl/ryCYqk
 
     Note the above syntax example and usage examples below to better understand how to
     use the function. Although the function is slightly more complicated to use than a
@@ -83,17 +88,17 @@ Developer Notes:
     unlike a scalar UDFs or multi-line table valued functions, the inline scalar UDF does
     not restrict the query optimizer's ability generate a parallel query execution plan.
 
- 3. patExclude8k generally performs better with a parallel execution plan
+ 4. patExclude8K_2017 generally performs better with a parallel execution plan
  
- 4. @pattern is not case sensitive (the function can be easily modified to make it so)
+ 5. @pattern is not case sensitive (the function can be easily modified to make it so)
 
- 5. There is no need to include the "%" before and/or after your pattern since since we 
+ 6. There is no need to include the "%" before and/or after your pattern since since we 
 	  are evaluating each character individually
 
- 6. Latin1_General_BIN used in the PATINDEX statement improves performance and causes the
+ 7. Latin1_General_BIN used in the PATINDEX statement improves performance and causes the
     function to perform case sensitive comparisons. This can be safely removed or changed.
  
- 7. patExclude8K is deterministic; for more about deterministic and nondeterministic 
+ 8. patExclude8K_2017 is deterministic; for more about deterministic and nondeterministic 
     functions see https://msdn.microsoft.com/en-us/library/ms178091.aspx
 
 ----------------------------------------------------------------------------------------
@@ -123,13 +128,7 @@ Revision History:
  Rev 06 - 20171214 - Alan Burstein - Added ORDER BY clause, updated documentation
 *****************************************************************************************/
 RETURNS TABLE WITH SCHEMABINDING AS RETURN
-SELECT newString =
-(
-  SELECT ng.token+'' -- token+'' + PATH('') + no ROOT clause only returns the token (no xml)
-  FROM dbo.ngrams8k(@string,1) ng
-  WHERE 0 = PATINDEX(@pattern, ng.token COLLATE Latin1_General_BIN)
-  ORDER BY ng.position   -- your spoon
-  FOR XML PATH(''), TYPE -- TYPE & value clauses handle special XML characters such as "&"
-).value('text()[1]','varchar(8000)'); -- Return value data type; change as needed
+SELECT newString = STRING_AGG(ng.token,'') WITHIN GROUP (ORDER BY position) -- your spoon
+FROM dbo.ngrams8k(@string,1) ng
+WHERE 0 = PATINDEX(@pattern, token COLLATE Latin1_General_BIN);
 GO
-
