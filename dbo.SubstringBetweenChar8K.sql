cc7ff5866d70d36e37715b25d4ee2907bb02aaa1
Alan Burstein <alanjburstein@gmail.com>
2018-06-13 22:57:43 -0500

    Updated dbo.SubstringBetweenChar8K
    Some performance improvements and bug fixes. Includes functionality to include varchar(100) delimiters (vs char(1))

--- dbo.SubstringBetweenChar8K.sql
+++ dbo.SubstringBetweenChar8K.sql
@@ -1,4 +1,4 @@
-USE sqlDevToolboxAB
+USE sqlDevToolboxAB2
 GO
 IF OBJECT_ID('dbo.SubstringBetweenChar8K') IS NOT NULL 
   DROP FUNCTION dbo.SubstringBetweenChar8K;
@@ -6,15 +6,15 @@ GO
 CREATE FUNCTION dbo.SubstringBetweenChar8K
 (
   @string    varchar(8000),
-  @start     tinyint,
-  @stop      tinyint,
-  @delimiter char(1)
+  @first     int,
+  @last      int,
+  @delimiter varchar(100)
 )
 /*****************************************************************************************
 Purpose:
  Takes in input string (@string) and returns the text between two instances of a delimiter
- (@delimiter); the location of the delimiters is defined by @start and @stop.
- For example: if @string = 'xx.yy.zz.abc', @start=1, @stop=3, and @delimiter = '.' the
+ (@delimiter); the location of the delimiters is defined by @first and @last.
+ For example: if @string = 'xx.yy.zz.abc', @first=1, @last=3, and @delimiter = '.' the
  function will return the text: yy.zz; this is the text between the first and third
  instance of "." in the string "xx.yy.zz.abc".
 
@@ -23,29 +23,29 @@ Compatibility:
 
 Syntax:
 --===== Autonomous use
- SELECT sb.token, sb.position, sb.tokenLength
- FROM dbo.SubstringBetweenChar8K(@string, @start, @stop, @delimiter); sb;
+ SELECT sb.item, sb.itemIndex, sb.itemLength
+ FROM dbo.SubstringBetweenChar8K(@string, @first, @last, @delimiter); sb;
 
 --===== Use against a table
- SELECT sb.token, sb.position, sb.tokenLength
+ SELECT sb.item, sb.itemIndex, sb.itemLength
  FROM SomeTable st
  CROSS APPLY dbo.SubstringBetweenChar8K(st.SomeColumn1, 1, 2, '.') sb;
 
 Parameters:
  @string    = varchar(8000); Input string to parse
- @start     = tinyint; the instance of @delimiter to search for; this is where the output 
-              should start. When @start is 0 then the function will return everything from
+ @first     = int; the instance of @delimiter to search for; this is where the output 
+              should start. When @first is 0 then the function will return everything from
               the beginning of @string until @end.
- @stop      = tinyint; the last instance of @delimiter to search for; this is where the
-              output should end. When @end is 0 then the function will return everything
-              from @start until the end of the string.
- @delimiter = char(1); this is the delimiter use to determine where the output starts/ends
+ @last      = int; the last instance of @delimiter to search for; this is where the output 
+              should end. When @end is 0 then the function will return everything from 
+              @first until the end of the string.
+ @delimiter = varchar(100); The delimiter use to determine where the output starts/ends
 
 Return Types:
  Inline Table Valued Function returns:
-   token     = varchar(8000); the substring between the two instances of @delimiter 
-               defined by @start and @stop
- position    = smallint; the location of where the substring begins
+   item     = varchar(8000); the substring between the two instances of @delimiter 
+               defined by @first and @last
+ itemIndex    = smallint; the location of where the substring begins
 ------------------------------------------------------------------------------------------
 Developer Notes:
  1. Requires NGrams8K. The code for NGrams8K can be found here:
@@ -65,52 +65,54 @@ Developer Notes:
     unlike a scalar UDFs or multi-line table valued functions, the inline scalar UDF does
     not restrict the query optimizer's ability generate a parallel query execution plan.
 
- 3. dbo.SubstringBetweenChar8K is deterministic; for more about deterministic and
+ 3. This function requires both the @first and @last parameters be positive integers.
+    When either is negative, the function will return NULLs.
+
+ 4. dbo.SubstringBetweenChar8K is deterministic; for more about deterministic and
     nondeterministic functions see https://msdn.microsoft.com/en-us/library/ms178091.aspx
 
 Examples:
+ DECLARE @string varchar(100) = 'abc.defg.hi.jk.lmnop.qrs.tuv';
 -- beginning of string to 2nd delimiter, 2nd delimiter to end of the string
-DECLARE @string varchar(100) = 'abc.defg.hi.jk.lmnop.qrs.tuv';
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,0,2, '.');
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,2,0, '.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,0,2, '.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,2,0, '.');
 
 -- Between the 1st & 2nd, then 2nd & 5th delimiters
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,1,2, '.');
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,2,5, '.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,1,2, '.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,2,5, '.');
 
 -- dealing with NULLS, delimiters that don't exist and when @first = @last
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,2,10,'.');
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,1,NULL,'.');
-SELECT string=@string, token, position FROM dbo.SubstringBetweenChar8K(@string,NULL,1,'.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,2,10,'.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,1,NULL,'.');
+  SELECT string=@string, item, itemIndex FROM dbo.SubstringBetweenChar8K(@string,NULL,1,'.');
 ---------------------------------------------------------------------------------------
 Revision History:
  Rev 00 - 20160720 - Initial Creation - Alan Burstein
- Rev 01 - 20160821 - Re-wrote a single-char version (this); removed tokenLen
+ Rev 01 - 20180613 - Complete re-design, includeing multi-char delimiters
 ****************************************************************************************/
 RETURNS TABLE WITH SCHEMABINDING AS RETURN
-WITH
-chars AS
-(
- SELECT instance = 0, position = 0 WHERE @start = 0
- UNION ALL
- SELECT ROW_NUMBER() OVER (ORDER BY position), position
- FROM dbo.NGrams8k(@string,1)
- WHERE token = @delimiter
- UNION ALL
- SELECT -1, DATALENGTH(@string)+1 WHERE @stop = 0
-)
 SELECT 
-  token = SUBSTRING
-          (
-            @string,
-            MIN(position)+1,
-            NULLIF(MAX(position),MIN(position)) - MIN(position)-1
-          ),
-  position = CAST
-      		(
-            CASE WHEN NULLIF(MAX(position),MIN(position)) - MIN(position)-1 > 0
-            THEN MIN(position)+1 END AS smallint
-          )
-FROM chars
-WHERE instance IN (@start, NULLIF(@stop,0), -1);
+  item = 
+    CASE WHEN @first >= 0 AND @last >=0 THEN
+      CASE WHEN @first+@last=0 THEN @string
+           WHEN @last=0        THEN SUBSTRING(@string, p.mn+LEN(@delimiter), 8000)
+           WHEN @first<@last   THEN SUBSTRING(@string, p.mn+LEN(@delimiter), 
+                                      NULLIF(p.mx,p.mn)-p.mn-LEN(@delimiter)) END END,
+  itemIndex = 
+    CASE WHEN @first >= 0 AND @last >=0 THEN
+      CASE WHEN @first+@last=0 THEN 1
+           WHEN @last=0        THEN (p.mn+LEN(@delimiter))
+           WHEN @first<@last   THEN (p.mn+LEN(@delimiter))*SIGN(NULLIF(p.mx,p.mn)) END END
+FROM
+(
+  SELECT MIN(d.de), MAX(d.de)
+  FROM
+  (
+    SELECT CHECKSUM(0),0 WHERE @first = 0 UNION ALL
+    SELECT CHECKSUM(ROW_NUMBER() OVER (ORDER BY ng.position)), ng.position
+    FROM dbo.ngrams8k(@string, LEN(@delimiter)) ng
+    WHERE ng.token = @delimiter
+  ) d(ds,de)
+  WHERE ds IN (@first,@last)
+) p(mn,mx);
 GO
